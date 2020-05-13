c19fb88edd72912ed857a303ae4f337589ec95a6
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-05-13 12:38:53 -0700

    Find the Team Size

--- Database/Find the Team Size.sql
+++ Database/Find the Team Size.sql
@@ -1,7 +1,12 @@
-sub query solution
+-- SUB QUERY solution
 SELECT e1.employee_id,
        COUNT(e2.employee_id) as "team_size"
 FROM Employee as e1
 LEFT JOIN Employee as e2
 ON e1.team_id = e2.team_id
 GROUP BY e1.employee_id 
+
+-- PARTITION solution
+SELECT employee_id,
+       COUNT(employee_id) OVER (PARTITION BY team_id) as "team_size"
+FROM Employee
