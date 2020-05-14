07e65c619473c0ed33bad8339378d80ad85ee374
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-05-14 15:10:05 -0700

    Minor change

--- Algorithms/Split a String in Balanced Strings.py
+++ Algorithms/Split a String in Balanced Strings.py
@@ -5,10 +5,10 @@ class Solution:
         # We need to count all the strings 
         count = 0
         for i in (s):
-            # If we see L we're going to increment the count and if we see R we're going to decrement the count.
-            if i == 'L':
-                count += 1
+            # If we see R we're going to increment the count and if we see L we're going to decrement the count.
             if i == 'R':
+                count += 1
+            if i == 'L':
                 count -= 1
             # If our count is 0 it means that we have an equal balance R and L
             if count == 0:
