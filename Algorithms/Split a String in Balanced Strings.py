817e08673b80cb7429204949a6dfe857a2b5a203
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-05-14 15:08:06 -0700

    Split a String in Balanced Strings

---
+++ Algorithms/Split a String in Balanced Strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        # We want to return the max balanced string
+        max_balanced = 0
+        # We need to count all the strings 
+        count = 0
+        for i in (s):
+            # If we see L we're going to increment the count and if we see R we're going to decrement the count.
+            if i == 'L':
+                count += 1
+            if i == 'R':
+                count -= 1
+            # If our count is 0 it means that we have an equal balance R and L
+            if count == 0:
+                max_balanced += 1
+        return max_balanced
