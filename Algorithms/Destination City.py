0ff8d0def58150372cde79f3632992fe628b3a9a
https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSDSDG201907DATA2.git <ruddy.simonpour@gmail.com>
2020-05-15 16:36:40 -0700

    Added 2nd solution to Destination City

--- Algorithms/Destination City.py
+++ Algorithms/Destination City.py
@@ -9,3 +9,10 @@ class Solution:
         for i, j in dic.items():
                 if j not in dic:
                     return j
+
+# List Solution
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start_city = set([path[0] for path in paths])
+        end_city = set([path[1] for path in paths])
+        return list(end_city - start_city)[0]
